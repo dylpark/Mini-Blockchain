a16fd3a069fba3bcb0a7783c1c900d3133a102c3
Achterom <achterom@Achteroms-MacBook-Pro.local>
2021-05-04 20:22:21 +1000

    creating the default constructor for the Block class

---
+++ block.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+print(datetime.now())
+
+class Block:
+  def __init__(self, transactions, previous_hash, nonce = 0):
+    self.transactions = transactions
+    self.previous_hash = previous_hash
+    self.nonce = nonce
+    self.timestamp = datetime.now()
